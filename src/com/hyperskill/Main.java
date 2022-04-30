fc1a69266161712804a36b4950de5b7c839d1855
wbryon <habibula74@gmail.com>
2022-04-30 22:52:57 +0300

    added DoCoffeeDrink.java

--- src/com/hyperskill/Main.java
+++ src/com/hyperskill/Main.java
@@ -10,67 +10,57 @@ public class Main {
         String coffeeType;
         String action;
 
-//        System.out.println("The coffee machine has:\n" + addIngredients.getWater() + " ml of water\n" + addIngredients.getMilk() + " ml of milk\n" +
-//                status.getCoffee() + " g of coffee beans\n" + addIngredients.getCupsAmount() + " disposable cups\n" + addIngredients.getMoney() + " of money\n" +
-//                "\nWrite action (buy, fill, take, remaining, exit):");
-
         while (true) {
             System.out.println("Write action (buy, fill, take, remaining, exit):");
             action = scanner.next();
-            if (action.equals("buy")) {
-                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
-                coffeeType = scanner.next();
-                if (coffeeType.equals("1")) {
-                    status.setWater(250);
-                    status.setCoffee(16);
-                    status.setMoney(4);
-                    status.setCupsAmount(1);
-                    System.out.println("\nI have enough resources, making you a coffee!");
+            switch (action) {
+                case "buy" -> {
+                    System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+                    coffeeType = scanner.next();
+                    if (coffeeType.equals("1")) {
+                        status.setWater(250);
+                        status.setCoffee(16);
+                        status.setMoney(4);
+                        status.setCupsAmount(1);
+                        System.out.println("\nI have enough resources, making you a coffee!");
+                    }
+                    if (coffeeType.equals("2")) {
+                        status.setWater(350);
+                        status.setMilk(75);
+                        status.setCoffee(20);
+                        status.setMoney(7);
+                    }
+                    if (coffeeType.equals("3")) {
+                        status.setWater(200);
+                        status.setMilk(100);
+                        status.setCoffee(12);
+                        status.setMoney(6);
+                    }
+                    if (coffeeType.equals("back")) {
+                        System.out.println("\n1The coffee machine has:\n" + addIngredients.getWater() + " ml of water\n" + addIngredients.getMilk() + " ml of milk\n" +
+                                addIngredients.getCoffee() + " g of coffee beans\n" + addIngredients.getCupsAmount() + " disposable cups\n" + addIngredients.getMoney() + " of money\n" +
+                                "\nWrite action (buy, fill, take, remaining, exit):");
+                    }
                 }
-                if (coffeeType.equals("2")) {
-                    status.setWater(350);
-                    status.setMilk(75);
-                    status.setCoffee(20);
-                    status.setMoney(7);
+                case "fill" -> {
+                    System.out.println("\nWrite how many ml of water you want to add:");
+                    addIngredients.setWater(scanner.nextInt());
+                    System.out.println("Write how many ml of milk you want to add:");
+                    addIngredients.setMilk(scanner.nextInt());
+                    System.out.println("Write how many grams of coffee you want to add:");
+                    addIngredients.setCoffee(scanner.nextInt());
+                    System.out.println("Write how many disposable cups of coffee you want to add:");
+                    addIngredients.setCupsAmount(scanner.nextInt());
+                    System.out.println();
                 }
-                if (coffeeType.equals("3")) {
-                    status.setWater(200);
-                    status.setMilk(100);
-                    status.setCoffee(12);
-                    status.setMoney(6);
+                case "take" -> {
+                    System.out.println("\nI gave you $" + addIngredients.getMoney());
+                    addIngredients.setMoney(0);
                 }
-                if (coffeeType.equals("back")) {
-                    System.out.println("\n1The coffee machine has:\n" + addIngredients.getWater() + " ml of water\n" + addIngredients.getMilk() + " ml of milk\n" +
-                            addIngredients.getCoffee() + " g of coffee beans\n" + addIngredients.getCupsAmount() + " disposable cups\n" + addIngredients.getMoney() + " of money\n" +
-                            "\nWrite action (buy, fill, take, remaining, exit):");
-                }
+                case "remaining" -> System.out.println("\nThe coffee machine has:\n" + addIngredients.getWater() + " ml of water\n" + addIngredients.getMilk() + " ml of milk\n" +
+                        addIngredients.getCoffee() + " g of coffee beans\n" + addIngredients.getCupsAmount() + " disposable cups\n" + addIngredients.getMoney() + " of money\n");
+                case "exit" -> System.exit(0);
             }
-            else if (action.equals("fill")) {
-                System.out.println("\nWrite how many ml of water you want to add:");
-                addIngredients.setWater(scanner.nextInt());
-                System.out.println("Write how many ml of milk you want to add:");
-                addIngredients.setMilk(scanner.nextInt());
-                System.out.println("Write how many grams of coffee you want to add:");
-                addIngredients.setCoffee(scanner.nextInt());
-                System.out.println("Write how many disposable cups of coffee you want to add:");
-                addIngredients.setCupsAmount(scanner.nextInt());
-                System.out.println();
-            }
-            else if (action.equals("take")) {
-                System.out.println("\nI gave you $" + addIngredients.getMoney());
-                addIngredients.setMoney(0);
-            }
-            else if (action.equals("remaining")) {
-                System.out.println("\nThe coffee machine has:\n" + addIngredients.getWater() + " ml of water\n" + addIngredients.getMilk() + " ml of milk\n" +
-                addIngredients.getCoffee() + " g of coffee beans\n" + addIngredients.getCupsAmount() + " disposable cups\n" + addIngredients.getMoney() + " of money\n");
-            }
-            else if (action.equals("exit")) {
-                System.exit(0);
-            }
-//            addIngredients.setWater(status.getWater());
-//            addIngredients.setMilk(status.getMilk());
-//            addIngredients.setCoffee(status.getCoffee());
-//            addIngredients.setCupsAmount(status.getCupsAmount());
         }
     }
 }
