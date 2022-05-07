b8c7eaa6667d9a9f7a6f6ca8c8d5424f0bd0e8bf
wbryon <haibula74@gmail.com>
2022-05-07 18:39:54 +0300

    repair master

--- src/com/hyperskill/DoCoffeeDrink.java
+++ src/com/hyperskill/DoCoffeeDrink.java
@@ -2,105 +2,112 @@ package com.hyperskill;
 
 public class DoCoffeeDrink {
 
-    private int latteWater = 350;
-    private int latteMilk = 75;
-    private int latteCoffee = 20;
-    private int latteCost = 7;
-
-    private int espressoWater = 250;
-    private int espressoCoffee = 16;
     private int espressoCost = 4;
-
-    private int cappuccinoWater = 200;
-    private int cappuccinoMilk = 100;
-    private int cappuccinoCoffee = 16;
+    private int latteCost = 7;
     private int cappuccinoCost = 6;
+    private int espressoWater = 250;
+    private int latteWater = 350;
+    private int cappuccinoWater = 200;
+    private int espressoCoffee = 16;
+    private int latteCoffee = 20;
+    private int cappuccinoCoffee = 16;
+    private int latteMilk = 75;
+    private int cappuccinoMilk = 100;
+    private int disposableCup = 1;
 
-    public void setLatteWater(int latteWater) {
-        this.latteWater = latteWater;
-    }
-
-    public void setLatteMilk(int latteMilk) {
-        this.latteMilk = latteMilk;
-    }
-
-    public void setLatteCoffee(int latteCoffee) {
-        this.latteCoffee = latteCoffee;
+    public void setEspressoCost(int espressoCost) {
+        this.espressoCost = espressoCost;
     }
 
     public void setLatteCost(int latteCost) {
         this.latteCost = latteCost;
     }
 
+    public void setCappuccinoCost(int cappuccinoCost) {
+        this.cappuccinoCost = cappuccinoCost;
+    }
+
     public void setEspressoWater(int espressoWater) {
         this.espressoWater = espressoWater;
     }
 
-    public void setEspressoCoffee(int espressoCoffee) {
-        this.espressoCoffee = espressoCoffee;
-    }
-
-    public void setEspressoCost(int espressoCost) {
-        this.espressoCost = espressoCost;
+    public void setLatteWater(int latteWater) {
+        this.latteWater = latteWater;
     }
 
     public void setCappuccinoWater(int cappuccinoWater) {
         this.cappuccinoWater = cappuccinoWater;
     }
 
-    public void setCappuccinoMilk(int cappuccinoMilk) {
-        this.cappuccinoMilk = cappuccinoMilk;
+    public void setEspressoCoffee(int espressoCoffee) {
+        this.espressoCoffee = espressoCoffee;
+    }
+
+    public void setLatteCoffee(int latteCoffee) {
+        this.latteCoffee = latteCoffee;
     }
 
     public void setCappuccinoCoffee(int cappuccinoCoffee) {
         this.cappuccinoCoffee = cappuccinoCoffee;
     }
 
-    public void setCappuccinoCost(int cappuccinoCost) {
-        this.cappuccinoCost = cappuccinoCost;
+    public void setLatteMilk(int latteMilk) {
+        this.latteMilk = latteMilk;
     }
 
-    public int getLatteWater() {
-        return latteWater;
+    public void setCappuccinoMilk(int cappuccinoMilk) {
+        this.cappuccinoMilk = cappuccinoMilk;
     }
 
-    public int getLatteMilk() {
-        return latteMilk;
-    }
-
-    public int getLatteCoffee() {
-        return latteCoffee;
-    }
-
-    public int getLatteCost() {
-        return latteCost;
-    }
-
-    public int getEspressoWater() {
-        return espressoWater;
-    }
-
-    public int getEspressoCoffee() {
-        return espressoCoffee;
+    public void setDisposableCup(int disposableCup) {
+        this.disposableCup = disposableCup;
     }
 
     public int getEspressoCost() {
         return espressoCost;
     }
 
+    public int getLatteCost() {
+        return latteCost;
+    }
+
+    public int getCappuccinoCost() {
+        return cappuccinoCost;
+    }
+
+    public int getEspressoWater() {
+        return espressoWater;
+    }
+
+    public int getLatteWater() {
+        return latteWater;
+    }
+
     public int getCappuccinoWater() {
         return cappuccinoWater;
     }
 
-    public int getCappuccinoMilk() {
-        return cappuccinoMilk;
+    public int getEspressoCoffee() {
+        return espressoCoffee;
+    }
+
+    public int getLatteCoffee() {
+        return latteCoffee;
     }
 
     public int getCappuccinoCoffee() {
         return cappuccinoCoffee;
     }
 
-    public int getCappuccinoCost() {
-        return cappuccinoCost;
+    public int getLatteMilk() {
+        return latteMilk;
+    }
+
+    public int getCappuccinoMilk() {
+        return cappuccinoMilk;
+    }
+
+    public int getDisposableCup() {
+        return disposableCup;
     }
 }
