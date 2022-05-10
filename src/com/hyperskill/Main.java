e0f15ee31c529cb8e3c6fca85d64e1004169fcde
Khabibula Tamirbudaev <49209809+wbryon@users.noreply.github.com>
2022-05-10 18:19:26 +0300

    Update Main.java

--- src/com/hyperskill/Main.java
+++ src/com/hyperskill/Main.java
@@ -14,27 +14,28 @@ public class Main {
             System.out.println("Write action (buy, fill, take, remaining, exit):");
             action = scanner.next();
             switch (action) {
-                case "buy" -> {
-                    System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+                case "buy" : {
+                    System.out.println("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
                     coffeeType = scanner.next();
                     if (coffeeType.equals("1")) {
                         status.doEspresso();
+                        continue;
                     }
                     if (coffeeType.equals("2")) {
                         status.doLatte();
+                        continue;
                     }
                     if (coffeeType.equals("3")) {
                         status.doCappuccino();
+                        continue;
                     }
                     if (coffeeType.equals("back")) {
-                        System.out.println("\n1The coffee machine has:\n" + status.getWater() + " ml of water\n" + status.getMilk() + " ml of milk\n" +
-                                status.getCoffee() + " g of coffee beans\n" + status.getCupsAmount() + " disposable cups\n" + status.getMoney() + " of money\n" +
-                                "\nWrite action (buy, fill, take, remaining, exit):");
+                        continue;
                     }
 
                 }
 
-                case "fill" -> {
+                case "fill" : {
                     System.out.println("\nWrite how many ml of water you want to add:");
                     fill.setWater(scanner.nextInt());
                     status.fillWater(fill.getWater());
@@ -44,18 +45,23 @@ public class Main {
                     System.out.println("Write how many grams of coffee you want to add:");
                     fill.setCoffee(scanner.nextInt());
                     status.fillCoffee(fill.getCoffee());
-                    System.out.println("Write how many disposable cups of coffee you want to add:");
+                    System.out.println("Write how many disposable cups of coffee beans you want to add:");
                     fill.setCupsAmount(scanner.nextInt());
                     status.fillCupsAmount(fill.getCupsAmount());
                     System.out.println();
+                    continue;
                 }
-                case "take" -> {
-                    System.out.println("\nI gave you $" + status.getMoney());
+                case "take" : {
+                    System.out.println("\nI gave you $" + status.getMoney() + "\n");
                     status.takeMoney();
+                    continue;
                 }
-                case "remaining" -> System.out.println("\nThe coffee machine has:\n" + status.getWater() + " ml of water\n" + status.getMilk() + " ml of milk\n" +
-                        status.getCoffee() + " g of coffee beans\n" + status.getCupsAmount() + " disposable cups\n" + status.getMoney() + " of money\n");
-                case "exit" -> System.exit(0);
+                case "remaining" : {
+                    System.out.println("\nThe coffee machine has:\n" + status.getWater() + " ml of water\n" + status.getMilk() + " ml of milk\n" +
+                            status.getCoffee() + " g of coffee beans\n" + status.getCupsAmount() + " disposable cups\n$" + status.getMoney() + " of money\n");
+                    continue;
+                }
+                case "exit" : System.exit(0);
             }
         }
     }
